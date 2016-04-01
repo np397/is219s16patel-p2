dfc250316dffcd69e8e588c62b526ee25a5da7bf
np397 <nidhipatel062194@gmail.com>
2016-03-31 21:12:06 -0400

    Added the code required to change the src attribute when the swapPhoto is called

--- js/gallery.js
+++ js/gallery.js
@@ -31,17 +31,21 @@ function animate() {
 }
 
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
+// Counter for the mImages array
+var mCurrentIndex = 0;
 
 function swapPhoto() {
 	//Add code here to access the #slideShow element.
 	//Access the img element and replace its source
 	//with a new image from your images array which is loaded 
 	//from the JSON string
+	$("#photo").attr('src', mImages[mCurrentIndex].img);
+	mCurrentIndex++;
 	console.log('swap photo');
 }
 
-// Counter for the mImages array
-var mCurrentIndex = 0;
+mCurrentIndex++;
+
 
 // XMLHttpRequest variable
 var mRequest = new XMLHttpRequest();
